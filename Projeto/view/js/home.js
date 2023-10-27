3fb29ac3dff747942f34aaae2aa5383fb5f9f9e2
Faraó <116319334+reiDOvarchar@users.noreply.github.com>
2023-10-27 16:25:44 -0300

    Delete Projeto directory

--- Projeto/view/js/home.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $(document).on("click", "#teste", function () {
-        
-    });
-
-    $(document).on("click", ".menu", function () {
-        $(this).toggleClass('open');
-    })
-});
