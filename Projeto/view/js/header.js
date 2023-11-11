c7b6df7cbaa123b61ee6bea99467f4f6e2c977a7
Faraó <116319334+reiDOvarchar@users.noreply.github.com>
2023-11-11 20:24:46 -0300

    Delete Projeto directory

--- Projeto/view/js/header.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $(document).on("click", "#teste", function () {
-        $( "#menu" ).slideToggle(400);
-    });
-});
-
-function myFunction(x) {
-    x.classList.toggle("change");
-  }
